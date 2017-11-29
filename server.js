f5c9973acb05e3c7b169ab73a4f2b9d8bd451669
Uzaif <uzaif313@gmail.com>
2017-11-29 08:09:56 +0530

    intial app setup with handlebars template engine

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const request = require('request');
+const async = require('async');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const expressHbs = require('express-handlebars');
+//create express app
+const app  = express()
+
+//Setup port no
+const port = 8000
+
+// body parser middleware setup for read form data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+// setup template engine
+app.engine('.hbs', expressHbs({defaultLayout:'application',extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
+// morgan middleware setup inspect log
+app.use(morgan("dev"))
+
+app.get("/",(req ,res, next) => {
+  res.render("main/home")
+})
+
+app.listen(port,(err)=>{
+  if (err) {
+    console.log(err)
+  }else {
+    console.log(`Server is Running on ${port}`)
+  }
+})
