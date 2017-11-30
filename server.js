a6471ec67a7f0edf0014ebff0366bcdf96e5188f
Uzaif <uzaif313@gmail.com>
2017-11-30 08:08:28 +0530

    configure express static middleware and integrate barebones layout for application

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const port = 8000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+
+// static asset middleware
+app.use(express.static(__dirname+"/public"))
+
 // setup template engine
 app.engine('.hbs', expressHbs({defaultLayout:'application',extname:'.hbs'}))
 app.set('view engine', 'hbs')
